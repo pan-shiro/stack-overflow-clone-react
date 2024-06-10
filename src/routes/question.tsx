5451672764f4187c1b15f486f3e19fa559f005d0
Ioannis Potouridis <potouridisio@gmail.com>
2024-06-10 12:14:19 +0300

    Add answers and post answer form

--- src/routes/question.tsx
+++ src/routes/question.tsx
@@ -1,9 +1,11 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Chip from '@mui/material/Chip'
 import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import {
@@ -42,6 +44,52 @@ export default function Question() {
             })}
           </Stack>
         </CardContent>
+      </Card>
+
+      <Toolbar disableGutters>
+        <Typography component="div" variant="h6">
+          1 Answer
+        </Typography>
+      </Toolbar>
+
+      <Stack spacing={2}>
+        <Card>
+          <CardContent>
+            <Typography color="text.secondary">
+              One of the recommended ways to loop through an array in JavaScript
+              is to use the 'forEach' method. It provides a more concise syntax
+              and handles the iteration automatically. Here's an example:
+            </Typography>
+          </CardContent>
+        </Card>
+      </Stack>
+
+      <Toolbar disableGutters>
+        <Typography component="div" variant="h6">
+          Your Answer
+        </Typography>
+      </Toolbar>
+
+      <Card>
+        <CardContent>
+          <form id="new-answer" method="post">
+            <TextField fullWidth multiline name="body" rows={4} />
+            {/* Body is missing. */}
+            {/* Body must be at least 30 characters; you entered ${body.length}. */}
+          </form>
+        </CardContent>
+
+        <CardActions>
+          <Button
+            form="new-answer"
+            name="intent"
+            size="small"
+            type="submit"
+            value="post-answer"
+          >
+            Post Your Answer
+          </Button>
+        </CardActions>
       </Card>
     </Box>
   )
